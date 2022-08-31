2493421e8cb27cd9ca9f7b5b4621934ae38df7cf
Jesse Dirisu <dirisujesse@gmail.com>
2022-08-31 23:02:20 +0100

    Initial Commit

---
+++ OmdbApp/OmdbAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  OmdbAppApp.swift
+//  OmdbApp
+//
+//  Created by Dirisu on 31/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct OmdbAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
